556e8bb728d163a18da063ab5e312dbbad2b8d81
jjh <jjh920415@naver.com>
2018-09-21 20:05:08 +0900

    .

--- commit.sh
+++ commit.sh
@@ -3,6 +3,7 @@ git submodule foreach git checkout master || exit 1
 git submodule foreach git add . || exit 1
 git submodule foreach git commit -m "." || exit 1
 git submodule foreach git push || exit 1
+git submodule update || exit 1
 git add . || exit 1
 git commit -m "." || exit 1
 git push 
